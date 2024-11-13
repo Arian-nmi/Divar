0b58ace8a6969a36db12695cc8cd6f3675d0d218
Arian-nmi <naeimiarian82@gmail.com>
2024-11-13 11:10:37 +0330

    models & views

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 
 ROOT_URLCONF = 'config.urls'
 
